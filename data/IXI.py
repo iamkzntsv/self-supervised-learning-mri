bcc89c7dcbd7b5f593af80a8a1df05e76a50a265
Sergey <kzntsv.mnsk@gmail.com>
2022-12-23 15:19:49 +0100

    Added DataLoader

--- data/IXI.py
+++ data/IXI.py
@@ -1,24 +1,26 @@
-import numpy as np
-import nibabel as nib
 from tqdm.auto import tqdm
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 from preprocessing.SliceExtractor import ext
+from preprocessing import preprocess
 from utils import *
 
 
 class IXI(Dataset):
 
-    def __init__(self, root_dir, transform=None):
-        self.root = root_dir
+    def __init__(self, root, transform=None):
+        self.root = root
         self.transform = transform
         self.samples = self._make_dataset()
 
     def __len__(self):
         return len(self.samples)
 
-    def __getitem__(self, idx):
-        # TO DO: implement method
-        pass
+    def __getitem__(self, index):
+        img = self.samples[index]
+        if self.transform is not None:
+            img = self.transform(img)
+        return img
 
     def _make_dataset(self):
         """
@@ -32,10 +34,23 @@ class IXI(Dataset):
             img_sliced = ext.get_slices(img)
             images.extend(img_sliced)
 
+        print("{} 2D slices collected from {} images.".format(len(images), len(fnames)))
+
         return images
 
-    def _get_loader(self):
-        # TO DO
-        pass
+    @staticmethod
+    def get_loader(root, batch_size, shuffle=True, num_workers=0, drop_last=False):
+
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+        ]) # Add transforms
+
+        dataset = IXI(root, transform)
+        return DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=shuffle,
+                          num_workers=num_workers,
+                          drop_last=drop_last
+                          )
 
 

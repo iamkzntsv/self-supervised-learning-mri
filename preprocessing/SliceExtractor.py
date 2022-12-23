1b67d5125ddfd6b29dc747eae38128185805779e
Sergey <kzntsv.mnsk@gmail.com>
2022-12-23 13:02:01 +0100

    Updated SliceExtractor

--- preprocessing/SliceExtractor.py
+++ preprocessing/SliceExtractor.py
@@ -1,4 +1,3 @@
-import nibabel as nib
 import numpy as np
 import tensorflow as tf
 from deepbrain import Extractor
@@ -14,58 +13,53 @@ class SliceExtractor:
 
     def get_slices(self, volume):
         """
-
-        :param volume: Nifti1Image representing mri volume of a single subject
-        :return:
+        Select 2D slices with relevant amounts of brain quantity from a 3D volume
+        :param volume: nifti image representing MRI volume of a single subject
+        :return: list of slices, where each slice is a numpy array of shape (256, 150)
         """
         nx, ny, nz = volume.header.get_data_shape()
 
         img_arr = volume.get_fdata()
-        img_arr_cropped = self.crop(img_arr)
-
-        """
-        idx = 125
-        plt.title("Slice {}, amount of brain quantity: {}".format(idx, np.squeeze(bq)[idx]))
-        plt.imshow(np.squeeze(img_arr_cropped[:,idx:idx+1,:]), cmap='gray')
-        plt.show()
-        """
-
-        exit()
+        img_arr = self.reduce(img_arr)
 
         # Loop over axial plane
         slices = []
         for i in range(ny - 1):
-            s = np.squeeze(img_arr[:,i:i+1,:])
-            slices.append(s)
+            s = np.squeeze(img_arr[:,i:i + 1,:])
+            s = np.pad(s, ((0, 0), (0, 150 - s.shape[1])))  # pad so that all slices have the same shape
+            if np.sum(s) > 0:
+                slices.append(s)
+                """
+                plt.imshow(s, cmap='gray')
+                plt.show()
+                """
 
         return slices
 
-    def crop(self, img_arr, th=8000):
+    def reduce(self, img_arr, th=15000):
         """
-        Compute brain quantity for each slice of the axial plane and remove parts where the amount of tissue is not big enough
+        Compute brain quantity for each slice of the axial plane and set irrelevant slices to 0
         :param img_arr: numpy array of shape (nx, ny, nz)
-        :param th: threshold on the amount of brain quantity needed for the slice to be considered relevant
+        :param th: amount of brain quantity needed for the slice to be considered relevant
         :return:
         """
-        # Perform skull stripping
+        # Skull stripping
         vox_mask = self.get_mask(img_arr) * 1
         img_arr_no_skull = img_arr * vox_mask
 
-        # Compute brain quantity (sum over coronal and sagittal axes)
+        # Compute brain quantity in axial plane (sum over coronal and sagittal axes)
         bq = np.apply_over_axes(np.sum, vox_mask, [0, 2])
         bq_mask = bq > th
 
         # Crop irrelevant parts of the volume
-        img_arr_cropped = img_arr_no_skull * bq_mask
-        # TO DO: remove 0 slices
-
-        return img_arr_cropped
+        img_arr_reduced = img_arr_no_skull * bq_mask
+        return img_arr_reduced
 
     def get_mask(self, img, th=0.5):
         """
-        Create a binary mask
+        Create a binary mask representing voxels with relevant amount of brain tissue
         :param img: image obtained after calling get_fdata() method, numpy array of shape (nx, ny, nz)
-        :param th: threshold on the amount of brain tissue in a single voxel
+        :param th: amount of brain tissue need for a single voxel to be considered relevant
         :return: binary mask for the input image, numpy array of shape (nx, ny, nz)
         """
         # Get probability for each voxel being a brain tissue
